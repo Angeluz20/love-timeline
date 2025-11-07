660a510b296a122d9805a2e2936bd632eddea0f8
estevao <estevao.silva@venturus.com>
2025-11-07 16:37:57 -0400

    feat: add new styled do App file main

---
+++ src/AppStyle.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ContainerMain = styled.div `
+    position: relative;
+    background: radial-gradient(circle at top left, #fffafc, #fbeff2);
+    width: 100vw;
+    height: 100vh;
+`
