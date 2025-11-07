d3aef4272198c579fe63d38701f935977540c27d
estevao <estevao.silva@venturus.com>
2025-11-07 16:39:44 -0400

    feat: adds dependencies project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import hearts from "./assets/heart-burst.json"
+import Lottie from "lottie-react";
+
 import './App.css';
+import Home from './screens/Home/HomePage';
+import { ContainerMain } from './AppStyle';
 
 function App() {
+      const cleanedAnimation = {
+      ...hearts,
+      layers: hearts.layers.filter(layer => layer.nm !== "background"),
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContainerMain>
+    <Home />
+    </ContainerMain>
+        
   );
 }
 
