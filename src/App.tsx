d6dd1aba39c73f6b8d0f412a29cc5a354d4bdcf2
estevao <estevao.silva@venturus.com>
2025-11-07 17:07:40 -0400

    fix: remove unused variables to pass ESLint in CI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
 import hearts from "./assets/heart-burst.json"
-import Lottie from "lottie-react";
 
 import './App.css';
 import Home from './screens/Home/HomePage';
 import { ContainerMain } from './AppStyle';
 
 function App() {
-      const cleanedAnimation = {
-      ...hearts,
-      layers: hearts.layers.filter(layer => layer.nm !== "background"),
-    };
   return (
     <ContainerMain>
     <Home />
